e58e30a469f7679b7a3d3e9c654df526c9c5e515
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-07-30 12:49:23 +0530

    create variables

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.button.MaterialButton;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION  = '-';
+    private static final char MULTIPLICATION  = '*';
+    private static final char DIVISION  = '/';
+    private static final char PERCENT  = '%';
+
+    private char currentSymbol;
+
+    private double firstValue = Double.NaN;
+    private double secondValue;
+    private TextView inputDisplay, outputDisplay;
+    private DecimalFormat decimalFormat;
+    private MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9,
+            buttonDot, buttonAdd, buttonSub, buttonMultiply, buttonDivide, buttonPercent, buttonClear, buttonOFF, buttonEqual;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
